293f22d4af19a139a37bcf6edfacdcf634ba0ab7
Yash Patil <ya88287946@gmail.com>
2026-02-23 22:08:34 +0530

    Implement centralized error handler in mainServer.js
    Added a centralized error handler to manage errors in the application.

--- mainServer.js
+++ mainServer.js
@@ -1,14 +1,22 @@
-const express = require('express');
+const express = require('express'); 
 const app= express();
 
 const productRoutes = require('./routes/productRoute')
 
-
 app.use(express.static('public'))
 app.use(express.json());
 
-
 app.use('/api/products', productRoutes)
+
+
+
+// Centralized Error Handler (ALWAYS LAST)
+app.use((err, req, res, next) => {
+    console.log("Error:", err.message);
+    res.status(err.status || 500).json({
+        message: err.message
+    });
+});
 
 
 
